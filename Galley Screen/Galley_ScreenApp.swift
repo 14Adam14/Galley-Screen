c75483a65ef2ce2d656cde0decdb899dfe73df60
Adam Kaum <adam.bolotov@gmail.com>
2024-05-10 04:21:48 +0400

    Initial Commit

---
+++ Galley Screen/Galley_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Galley_ScreenApp.swift
+//  Galley Screen
+//
+//  Created by Adam Kaum on 10.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Galley_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
